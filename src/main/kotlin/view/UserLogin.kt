979adb1a458585d792cd0389531309848992e4ae
ethanwatson <17watsoeth@gmail.com>
2018-12-01 15:36:35 -0500

    made login for existing users.

--- src/main/kotlin/view/UserLogin.kt
+++ src/main/kotlin/view/UserLogin.kt
@@ -2,36 +2,63 @@ package view
 
 import tornadofx.*
 
-
-//class UserLogin : View() {
-//    override val root = vbox {
-//        button("Press me")
-//        label("Waiting")
-//    }
-//}
-
-
 class UserLogin: View() {
-    override val root = vbox {
-        hbox {
-            label("User Login Page")
+
+//    override val root = form {
+//        fieldset(labelPosition = Orientation.VERTICAL) {
+//            field("Username") { textfield() }
+//            field("Password") { passwordfield() }
+//            button("Log in") {
+//                action { println("Handle button press") }
+//            }
+//        }
+//    }
+
+    // vbox = vertical, hbox = horizontal
+    override val root = hbox {
+        // new user
+        vbox {
+            hbox {
+                label("User Login Page")
+            }
+            hbox {
+                label("Email")
+                textfield()
+            }
+            hbox {
+                label("Full Name:")
+                textfield()
+            }
+            hbox {
+                label("Optional Phone Number")
+                textfield()
+            }
+            hbox {
+                label("Scientist?")
+                checkbox {  }
+            }
+            button("Create user") {
+                useMaxWidth = true
+                action {
+                    //TODO("validate user")
+                    replaceWith<Home>()
+                    println("handle creating a user")
+                    // should validate this info with database, then store if okay to do.
+                }
+            }
         }
-        hbox {
-            label("Email")
-            textfield()
-        }
-        hbox {
-            label("Full Name:")
-            textfield()
-        }
-        hbox {
-            label("Optional Phone Number")
-            textfield()
-        }
-        button("Login") {
-            useMaxWidth = true
-            action {
-                replaceWith<Home>()
+        // login existing user
+        vbox {
+            hbox {
+                label("Email ")
+                textfield()
+            }
+            button("Login") {
+                action {
+                    //TODO("handle login")
+                    replaceWith<Home>()
+                    println("handle logging in")
+                }
             }
         }
     }
@@ -39,6 +66,10 @@ class UserLogin: View() {
 
 class Home: View() {
     override val root = vbox {
+        hbox {
+            label("Welcome to Animal Tracking")
+        }
+
         button("Back") {
             action {
                 replaceWith<UserLogin>()
