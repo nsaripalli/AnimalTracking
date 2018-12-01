f0a6293914ddd875f1df545868df17fdddf35255
Tanner Muro <tmuro17@gmail.com>
2018-12-01 16:19:42 -0500

    Added data classes to pass data around as

--- src/main/kotlin/controller/DbTesting.kt
+++ src/main/kotlin/controller/DbTesting.kt
@@ -3,15 +3,17 @@ package controller
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.Table
 import org.joda.time.DateTime
+import java.sql.Blob
 
-object Species : Table() {
+object SpeciesTable : Table() {
     val speciesID = integer("species_id").autoIncrement().primaryKey()
     val taxon = varchar("taxon", 100)
     val commonName = varchar("common_name", 250).nullable()
     val scientificName = varchar("scientific_name", 250)
 }
 
-object Observer : Table() {
+
+object ObserverTable : Table() {
     val observerID = integer("observer_id").autoIncrement().primaryKey()
     val name = varchar("name", 150)
     val email = varchar("email", 150)
@@ -19,29 +21,66 @@ object Observer : Table() {
     val isScientist = bool("scientist").default(false)
 }
 
-object Watch : Table() {
+object WatchTable : Table() {
     val watchID = integer("watch_id").autoIncrement().primaryKey()
     val latitude = double("latitude")
     val longitude = double("longitude")
     val radius = double("radius").default(1.0)
     val startDate = datetime("start_date").default(DateTime.now())
     val endDate = datetime("end").nullable()
-    val speciesID = integer("species_id").references(Species.speciesID)
-    val scientistID = integer("scientist_id").references(Observer.observerID)
+    val speciesID = integer("species_id").references(SpeciesTable.speciesID)
+    val scientistID = integer("scientist_id").references(ObserverTable.observerID)
 }
 
-object Sighting : Table() {
+object SightingTable : Table() {
     val sightingID = integer("sighting_id").autoIncrement().primaryKey()
     val quantity = integer("quantity").default(1)
     val latitude = double("latitude")
     val longitude = double("longitude")
     val notes = varchar("notes", 500).nullable()
-    val speciesID = integer("species_id").references(Species.speciesID)
-    val observerID = integer("observer_id").references(Observer.observerID)
+    val speciesID = integer("species_id").references(SpeciesTable.speciesID)
+    val observerID = integer("observer_id").references(ObserverTable.observerID)
     val photo = blob("photo").nullable()
-    val watchID = integer("watch_id").references(Watch.watchID).nullable()
+    val watchID = integer("watch_id").references(WatchTable.watchID).nullable()
 }
 
+data class Species(
+    val speciesID: Int,
+    val taxon: String,
+    val commonName: String?,
+    val scientificName: String
+)
+
+data class Observer(
+    val observerID: Int,
+    val name: String,
+    val email: String,
+    val phone: String?,
+    val isScientist: Boolean = false
+)
+
+data class Watch(
+    val watchID: Int,
+    val latitude: Double,
+    val longitude: Double,
+    val radius: Double = 1.0,
+    val startDate: DateTime = DateTime.now(),
+    val endDate: DateTime?,
+    val species: Species,
+    val scientist: Observer
+)
+
+data class Sighting(
+    val sightingID: Int,
+    val quantity: Int = 1,
+    val latitude: Double,
+    val longitude: Double,
+    val notes: String?,
+    val species: Species,
+    val observer: Observer,
+    val photo: Blob, //I dont know if blob is the right type, but it will work for now
+    val watch: Watch?
+)
 
 fun main(args: Array<String>) {
     Database.connect(
