31830ac7b29834793119adc42d48869e2bb46026
nsaripalli@hotmail.com <nsaripalli>
2018-12-09 16:39:48 -0500

    Changed the location of the SQL statements

--- Create.sql
+++ Create.sql
@@ -11,10 +11,7 @@ CREATE TABLE IF NOT EXISTS species
   common_name     VARCHAR(250),
   scientific_name VARCHAR(250) NOT NULL UNIQUE
 );
-#Additionally use DataGrip to map all of the species in the species.csv into the table
-insert into species (taxon, common_name, scientific_name)
-values ('small bird', 'red bird', 'cardinal'),
-       ('big bird', 'yellow bird', 'fromsesame');
+
 
 CREATE TABLE IF NOT EXISTS observer
 (
@@ -25,12 +22,6 @@ CREATE TABLE IF NOT EXISTS observer
   scientist   boolean DEFAULT FALSE
 );
 
-insert into observer (name, email, phone, scientist)
-values ('ethan', '17watsoeth@gmail.com', '111-111-1111', true),
-       ('tanner', 'tanner@gmail.com', '222-222-2222', true),
-       ('nithin', 'nithin@yahoo.com', '333-333-3333', true),
-       ('jeffrey', 'jeff@gmail.com', '444-444-4444', true),
-       ('rebeccaworker', 'guy@rebeccas.com', '555-555-5555', false);
 
 
 CREATE TABLE IF NOT EXISTS watch
@@ -47,9 +38,6 @@ CREATE TABLE IF NOT EXISTS watch
   FOREIGN KEY (scientist_id) REFERENCES observer (observer_id)
 );
 
-insert into watch (latitude, longitude, radius, start_date, end_date, species_id, scientist_id)
-values (10.0, 10.0, 1, '2018-12-01 10:00:00', '2018-12-05 10:00:00', 1, 1) -- ethan creates a watch for small bird.
-;
 
 
 CREATE TABLE IF NOT EXISTS sighting
@@ -68,6 +56,3 @@ CREATE TABLE IF NOT EXISTS sighting
   FOREIGN KEY (observer_id) REFERENCES observer (observer_id),
   FOREIGN KEY (watch_id) REFERENCES watch (watch_id)
 );
-
-insert into sighting (latitude, longitude, notes, species_id, observer_id, photo, watch_id)
-values (1, 1, 'Rachlin did not find whales', 1, 1, null, 1);
--- testData.sql
+++ testData.sql
@@ -1,1 +1,20 @@
 -- insert
+#Additionally use DataGrip to map all of the species in the species.csv into the table
+insert into species (taxon, common_name, scientific_name)
+values ('small bird', 'red bird', 'cardinal'),
+       ('big bird', 'yellow bird', 'fromsesame');
+
+insert into observer (name, email, phone, scientist)
+values ('ethan', '17watsoeth@gmail.com', '111-111-1111', true),
+       ('tanner', 'tanner@gmail.com', '222-222-2222', true),
+       ('nithin', 'nithin@yahoo.com', '333-333-3333', true),
+       ('jeffrey', 'jeff@gmail.com', '444-444-4444', true),
+       ('rebeccaworker', 'guy@rebeccas.com', '555-555-5555', false);
+
+insert into watch (latitude, longitude, radius, start_date, end_date, species_id, scientist_id)
+values (10.0, 10.0, 1, '2018-12-01 10:00:00', '2018-12-05 10:00:00', 1, 1) -- ethan creates a watch for small bird.
+;
+
+
+insert into sighting (latitude, longitude, notes, species_id, observer_id, photo, watch_id)
+values (1, 1, 'Rachlin did not find whales', 1, 1, null, 1);
